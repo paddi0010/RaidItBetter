fccad39f90f6fc9d68de62940b62c2846b368dcc
paddi0010 <kontakt.paddi001@gmail.com>
2026-09-05 19:59:52 +0200

    Exclude config file from version control

--- config.py
+++
@@ -1,5 +0,0 @@
-CLIENT_ID = "cby2ow7v5oom6crkbftbbs6l9354yc"
-CLIENT_SECRET = "efbbtw09bfbfvao0pugxcwvndn1ijb"  # Hier deine Twitch Client ID eintragen
-REDIRECT_URI = "http://localhost:3000/callback"
-FAVS_FILE = "favorites.json"
-CONFIG_FILE = "config.json"
